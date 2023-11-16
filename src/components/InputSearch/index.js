3fe471e2804a15d0283bd99c0a253cf1bb97011a
PramudyaReynaldi <prmdy02@gmail.com>
2023-11-17 02:22:21 +0700

    modified: update search input

--- src/components/InputSearch/index.js
+++ src/components/InputSearch/index.js
@@ -1,14 +1,21 @@
+// components/InputSearch.js
 import { MagnifyingGlass } from "@phosphor-icons/react";
 
-const InputSearch = ({ fowardRef, placeholder, onClick }) => {
+const InputSearch = ({ value, onChange, placeholder, onClick }) => {
     return (
         <div className="border md:my-0 lg:my-0 my-3 flex items-center p-2 rounded-md">
-            <input type="search" placeholder={placeholder} className="outline-none px-2" ref={fowardRef} />
+            <input
+                type="search"
+                placeholder={placeholder}
+                className="outline-none px-2"
+                value={value}
+                onChange={onChange} // Update added here
+            />
             <button onClick={onClick}>
                 <MagnifyingGlass size={20} />
             </button>
         </div>
-    )
+    );
 };
 
 export default InputSearch;
--- src/components/utils/Navbar/index.js
+++ src/components/utils/Navbar/index.js
@@ -1,31 +1,47 @@
 "use client";
-import { useRef } from "react";
+
+import { useState } from "react";
 import { useRouter } from "next/navigation";
-import InputSearch from "../../InputSearch";
+import InputSearch from "@/components/InputSearch";
+import debounce from "lodash/debounce";
 
 const Navbar = () => {
-    // Mengakses nilai dari element input yang terkait dengan search
-    const searchRef = useRef();
+    const [searchQuery, setSearchQuery] = useState("");
     const router = useRouter();
-    
-    const handleSearch = (event) => {
-        event.preventDefault();
-        // Mengambil value dari input (useRef())
-        const keyword = searchRef.current.value;
+    // const searchRef = useRef();
 
-        const destination = keyword === "" ? '/' : `/search/${keyword}`;
-        keyword === "" ? alert("Masukkan kata kunci") : router.push(destination);
-    }
-    
+    // Debounce the handleSearch function to avoid making too many requests
+    const debouncedSearch = debounce((query) => handleSearch(query), 300);
+
+    const handleSearch = (query) => {
+        const keyword = query.trim(); // Remove leading and trailing whitespaces
+        const destination =
+            keyword === "" ? "/" : `/search/${encodeURIComponent(keyword)}`;
+
+        if (keyword !== "") return router.push(destination);
+    };
+
+    const handleChange = (e) => {
+        const newQuery = e.target.value;
+        setSearchQuery(newQuery);
+        debouncedSearch(newQuery);
+    };
+   
     return (
         <header className="bg-white">
             <div className="flex md:flex-row lg:flex-row flex-col justify-between items-center p-5 shadow-xl">
-                <h2 className="text-3xl font-bold text-black">Anime<span className="text-red-500">Movie</span></h2>
-                <InputSearch fowardRef={searchRef} placeholder="Search..." onClick={handleSearch} />
+                <h2 className="text-3xl font-bold text-black">
+                    Anime<span className="text-red-500">Movie</span>
+                </h2>
+                <InputSearch
+                    value={searchQuery}
+                    onChange={handleChange}
+                    placeholder="Search..."
+                    onClick={() => debouncedSearch(searchQuery)}
+                />
             </div>
         </header>
-           
-    )
-}
+    );
+};
 
 export default Navbar;
