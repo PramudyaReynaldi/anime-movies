57f6b23dd097f977fbea71121f5d34b0322cbde7
PramudyaReynaldi <prmdy02@gmail.com>
2023-11-20 03:12:54 +0700

    modified: add decodedURIComponent

--- src/app/search/[keyword]/page.js
+++ src/app/search/[keyword]/page.js
@@ -2,33 +2,30 @@ import AnimeListCard from "@/components/AnimeListCard";
 import AnimeListTitle from "@/components/AnimeListCard/AnimeListTile";
 
 const Page = async ({ params }) => {
-    const { keyword } = params;
-    // atau
-    // const keyword = params.keyword;
+    const { keyword } = params; // atau const keyword = params.keyword;
+    const encodedKeyword = encodeURIComponent(keyword);  // Mengonversi nilai keyword menggunakan encodeURIComponent
 
     // Fetching API search anime
-    const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${keyword}`
+    const res = await fetch( 
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${encodedKeyword}`
     );
 
     const searchAnime = await res.json();
-    if(keyword === "") {
+    const decodedKeyword = decodeURIComponent(keyword); // Memastikan nilai 'keyword' di decode sebelum digunakan dalam URL
+
+    if(decodedKeyword === "") {
         return (
-            <>
-                <section className="home-container">
-                    <AnimeListTitle title="Anime Tidak Ditemukan" linkHref="/popular" linkText="See More" />
-                </section>
-            </>
+            <section className="home-container">
+                <AnimeListTitle title="Anime Tidak Ditemukan" linkHref="/popular" linkText="See More" />
+            </section>
         );
     }
 
     return (
-        <>
-            <section className="home-container">
-                <AnimeListTitle title={`Pencarian dari anime ${keyword} ...`} linkHref="/popular" linkText="See More" />
-                <AnimeListCard api={searchAnime} />
-            </section>
-        </>
+        <section className="home-container">
+            <AnimeListTitle title={`Pencarian dari anime ${decodedKeyword} ...`} linkHref="/popular" linkText="See More" />
+            <AnimeListCard api={searchAnime} />
+        </section>
     );
 };
 
