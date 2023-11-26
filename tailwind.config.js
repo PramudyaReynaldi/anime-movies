8d4ae5a5fb7eb06de58b4eec531d1569f21c0268
PramudyaReynaldi <prmdy02@gmail.com>
2023-11-26 14:45:34 +0700

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      backgroundColor: {
+        'secondary-bg': 'var(--secondary-bg)',
+        'primary-bg': 'var(--primary-bg)',
+      },
+      colors: {
+        'primary-color': 'var(--primary-color)',
+        'secondary-color': 'var(--secondary-color)',
+      }
     },
   },
   plugins: [],
