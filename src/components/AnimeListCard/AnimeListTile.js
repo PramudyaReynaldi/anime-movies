b06362d7cf9bed2279c21a3994db0844a5229827
PramudyaReynaldi <prmdy02@gmail.com>
2023-11-26 23:28:47 +0700

    modified: add container and change text

--- src/components/AnimeListCard/AnimeListTile.js
+++ src/components/AnimeListCard/AnimeListTile.js
@@ -2,13 +2,15 @@ import Link from "next/link";
 
 const AnimeListTitle = ({ title, titleSpan, linkHref, linkText }) => {
     return (
-        <div className="flex justify-between items-center p-6 pb-0">
-            <div className="text-xl font-bold text-primary-color">
-                <span className="text-secondary-color">{title}</span>{titleSpan}
+        <div className="container">
+            <div className="flex justify-between items-center p-6 pb-0">
+                <div className="text-text-title font-bold text-primary-color anime-title ps-2">
+                    <h5 className="font-oswald"><span className="text-secondary-color">{title}</span>{titleSpan}</h5>
+                </div>
+                <Link href={linkHref} className="text-sm hover-link text-primary-color">
+                    {linkText}
+                </Link>
             </div>
-            <Link href={linkHref} className="text-sm hover-link text-primary-color">
-                {linkText}
-            </Link>
         </div>
     );
 };
